db4a3bbdeee16f3c57ae1c1fe8ea5c1b91116260
Nikita <nikitka_ugatu@mail.ru>
2022-11-04 21:57:10 +0300

    настройка записей

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,6 +13,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
+            <PostList/>
         </div>
     )
 }
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,7 +3,18 @@ import React from "react";
 const PostListItem = () => {
     return (
         <li className="app-list-item d-flex justify-content-between">
-
+            <span className="app-list-item-label">
+                создать тестовое приложение
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button className="btn-star btn-sm">
+                    <i className="fa fa-star"></i>
+                </button>
+                <button className="btn-trash btn-sm">
+                    <i className="fa fa-trash-o"></i>
+                </button>
+                <i className="fa fa-heart"></i>
+            </div>
         </li>
     )
 }
