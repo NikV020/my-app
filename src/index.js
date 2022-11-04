2f9e852903ca21b6344a669d3fd6942375bbf8f4
Nikita <nikitka_ugatu@mail.ru>
2022-11-04 21:04:01 +0300

    display on the page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './';
+import App from './components/app/';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<App />
+)
