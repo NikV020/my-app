1f0fc1162492d09c902d624235921125c2d0ba87
Nikita <nikitka_ugatu@mail.ru>
2022-11-04 21:30:30 +0300

    настройка приложения

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,16 @@
 import React from "react";
+import AppHeader from "../app-header/app-header";
+import SearchPanel from "../search-panel/search-panel";
 
 const App = () => {
-    return (<h1>start new App</h1>)
+    return (
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+            </div>
+        </div>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/app/';
+import App from './components/app/app';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App />
+<App /> 
 )
