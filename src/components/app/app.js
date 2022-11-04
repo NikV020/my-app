a4476af5c289a35cc44cefb9d7c526ad24d173f8
Nikita <nikitka_ugatu@mail.ru>
2022-11-04 21:50:00 +0300

    adding styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import AppHeader from "../app-header/app-header";
 import SearchPanel from "../search-panel/search-panel";
 import PostStatusFilter from "../post-status-filter/post-status-filter";
-
+import PostList from "../post-list/post-list";
 
 
 const App = () => {
---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+
+        </li>
+    )
+}
+
+export default PostListItem;
